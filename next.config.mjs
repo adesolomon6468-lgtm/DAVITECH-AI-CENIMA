cc160405e491b66eac77478212b6a993d2634a94
adesolomon6468-lgtm <adesolomon6468@gmail.com>
2026-08-24 15:52:01 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-export default nextConfig
+const nextConfig={reactStrictMode:true}; export default nextConfig;
